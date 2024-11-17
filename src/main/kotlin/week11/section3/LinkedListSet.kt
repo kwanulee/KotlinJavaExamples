84d6fb263eb1d9fda964f8c2d33ea8a858bdccb7
kwanulee <kwlee@hansung.ac.kr>
2024-11-17 11:47:36 +0900

    directory changed

---
+++ src/main/kotlin/week11/section3/LinkedListSet.kt
@@ -0,0 +1,12 @@
+package week11.section3
+
+fun main() {
+    // Linked List를 이용한 HashSet
+    val intsLinkedHashSet: java.util.LinkedHashSet<Int> = linkedSetOf(35, 21, 76, 26, 75)
+    intsLinkedHashSet.add(4)
+    intsLinkedHashSet.remove(21)
+
+    println("intsLinkedHashSet $intsLinkedHashSet")
+    intsLinkedHashSet.clear()
+    println(intsLinkedHashSet)
+}
